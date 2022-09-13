7d7ad744dd6f14d6ff70961994dd058a43b5dc25
Onur ÖZEL <1030516465@erciyes.edu.tr>
2022-09-14 01:06:36 +0300

    Brick Interface and Enum Created

---
+++ Assets/[GAME]/Scripts/Enums/BrickType.cs
@@ -0,0 +1,15 @@
+﻿#region Header
+// Developed by Onur ÖZEL
+#endregion
+
+namespace _GAME_.Scripts.Enums
+{
+    public enum BrickType
+    {
+        Red,
+        Green,
+        Blue,
+        Purple,
+        Yellow
+    }
+}
---
+++ Assets/[GAME]/Scripts/Interfaces/IBrick.cs
@@ -0,0 +1,23 @@
+﻿#region Header
+// Developed by Onur ÖZEL
+#endregion
+
+using _GAME_.Scripts.Enums;
+
+namespace _GAME_.Scripts.Interfaces
+{
+    public interface IBrick
+    {
+        #region Properties
+
+        public BrickType BrickType { get; set; }
+
+        #endregion
+
+        #region Public Method
+
+        public void Collect(params object[] args);
+
+        #endregion
+    }
+}
