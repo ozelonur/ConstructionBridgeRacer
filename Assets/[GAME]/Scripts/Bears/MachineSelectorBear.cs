1c35fb9472b8c6c2a27bb3e230f26d9bebe330e6
Onur ÖZEL <1030516465@erciyes.edu.tr>
2022-09-27 22:03:19 +0300

    Models Import

---
+++ Assets/[GAME]/Scripts/Bears/MachineSelectorBear.cs
@@ -0,0 +1,58 @@
+﻿#region Header
+// Developed by Onur ÖZEL
+#endregion
+
+using _ORANGEBEAR_.EventSystem;
+using UnityEngine;
+
+namespace _GAME_.Scripts.Bears
+{
+    public class MachineSelectorBear : Bear
+    {
+        #region Serialized Fields
+
+        [Header("Machine Models")][SerializeField]
+        private GameObject[] machineModels;
+        
+        
+        [Header("Select Machine")][SerializeField]
+        private MachineTypes machineType;
+
+        #endregion
+        #region Event Methods
+
+        protected override void CheckRoarings(bool status)
+        {
+            if (status)
+            {
+                Register(GameEvents.InitLevel, InitLevel);
+            }
+
+            else
+            {
+                UnRegister(GameEvents.InitLevel, InitLevel);
+            }
+        }
+
+        private void InitLevel(object[] args)
+        {
+            EnableMachineModel();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void EnableMachineModel()
+        {
+            foreach (var machineModel in machineModels)
+            {
+                machineModel.SetActive(false);
+            }
+            
+            machineModels[(int) machineType].SetActive(true);
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/[GAME]/Scripts/Bears/MachineTypes.cs
@@ -0,0 +1,20 @@
+﻿#region Header
+// Developed by Onur ÖZEL
+#endregion
+
+namespace _GAME_.Scripts.Bears
+{
+    public enum MachineTypes
+    {
+        AsphaltCompactor = 0,
+        AsphaltCompactor2 = 1,
+        AsphaltMillingMachine = 2,
+        AsphaltPaver = 3,
+        Bulldozer1 = 4,
+        Bulldozer2 = 5,
+        Excavator = 6,
+        Forklift = 7,
+        Grader = 8,
+        Truck = 9,
+    }
+}
